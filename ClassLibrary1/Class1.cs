65f3ee8297ac5a4b8c967e2a40008879ba55ae5a
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-02-16 19:19:42 +0200

    create MathHelper

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class MathHelper
+    {
+        public static int GetMax(int a, int b, int c)
+        {
+            return a > b ? (a > c ? a : c) : (b > c ? b : c);
+        }
+    }
+}
