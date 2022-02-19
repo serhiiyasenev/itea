53aea9f50f6d5088964e2a588351d018e3ca1186
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-02-19 10:39:46 +0200

    update

---
+++ CoreDB/Tables/Cities.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Cities]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[Title] NVARCHAR(100) NOT NULL
+)
---
+++ CoreDB/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] uniqueidentifier NOT NULL PRIMARY KEY DEFAULT(NEWID()),
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[LastName] NVARCHAR(100) NOT NULL,
+	[Birthday] DATE NOT NULL
+)
