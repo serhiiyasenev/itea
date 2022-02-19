62e7b48b5a9bb40f0963dc90a8cf4f8775b14033
serhiiyasenev <serhii.yasenev@gmail.com>
2022-02-19 10:01:38 +0200

    lesson 2 unit tests (#2)
    * add unit tests for MathHelper
    
    * remove Setup
    
    * upd
    
    Co-authored-by: Serhii Yasenev <serhii.yasenev@ihsmarkit.com>

---
+++ ClassLibrary1/MathHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathHelper
+    {
+        public static int GetMax(int a, int b, int c)
+        {
+            return a > b ? (a > c ? a : c) : (b > c ? b : c);
+        }
+
+        public static int GetMaxFromArray(int[] sourceArray)
+        {
+            if (sourceArray == null)
+            {
+                throw new ArgumentException("array should not be null");
+            }
+
+            if (sourceArray.Length == 0)
+            {
+                throw new ArgumentException("array should not be emp");
+            }
+
+            var value = 0;
+
+            foreach (var x in sourceArray)
+            {
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+    }
+}
