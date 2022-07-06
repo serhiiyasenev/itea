1f1067f40d6c87731e9f456385aaa5a80fe17113
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-07-06 22:27:35 +0300

    hide redendant id

--- src/CoreBL/Models/Car.cs
+++ src/CoreBL/Models/Car.cs
@@ -12,6 +12,6 @@ namespace CoreBL.Models
         [MaxLength(12)]
         public string RegistrationPlate { get; set; }
 
-        public Guid UserId { get; set; }
+        //public Guid UserId { get; set; }
     }
 }
